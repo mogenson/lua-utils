0d2c682fc0a17c6186f6642810ab4885923e32cf
Michael Mogenson <michael.mogenson@gmail.com>
2024-01-05 09:43:47 -0500

    nstextview and nstextfield and button for app.lua

--- app.lua
+++ app.lua
@@ -2,6 +2,16 @@ local objc = require("objc")
 local ffi = require("ffi")
 local bit = require("bit")
 
+-- Foundation types
+ffi.cdef([[
+typedef struct NSEdgeInsets {
+    CGFloat top;
+    CGFloat left;
+    CGFloat bottom;
+    CGFloat right;
+} NSEdgeInsets;
+]])
+
 -- AppKit constants
 local NSApplicationActivationPolicyRegular = ffi.new("NSInteger", 0)
 local NSBackingStoreBuffered = ffi.new("NSUInteger", 2)
@@ -12,8 +22,19 @@ local NSWindowStyleMaskClosable = ffi.new("NSUInteger", bit.lshift(1, 1))
 local NSWindowStyleMaskMiniaturizable = ffi.new("NSUInteger", bit.lshift(1, 2))
 local NSWindowStyleMaskResizable = ffi.new("NSUInteger", bit.lshift(1, 3))
 
+local NSStackViewGravityTop = ffi.new("NSInteger", 1)
+local NSStackViewGravityLeading = ffi.new("NSInteger", 1)
+local NSStackViewGravityCenter = ffi.new("NSInteger", 2)
+local NSStackViewGravityBottom = ffi.new("NSInteger", 3)
+local NSStackViewGravityTrailing = ffi.new("NSInteger", 3)
+
+local NSUserInterfaceLayoutOrientationHorizontal = ffi.new("NSInteger", 0)
+local NSUserInterfaceLayoutOrientationVertical = ffi.new("NSInteger", 1)
+
 local NO = ffi.new("BOOL", 0)
 local YES = ffi.new("BOOL", 1)
+
+local button_action_selector = "buttonClicked:"
 
 local function NSStr(str)
     return objc.NSString:stringWithUTF8String(str)
@@ -30,12 +51,13 @@ local AppDelegateClass = objc.newClass("AppDelegate")
 objc.addMethod(AppDelegateClass, "applicationShouldTerminateAfterLastWindowClosed:", "B@:",
     function(self, cmd)
         print("last window closed")
-        return ffi.new("BOOL", 1)
+        return YES
     end)
 
-objc.addMethod(AppDelegateClass, "myClicked:", "v@:@",
+objc.addMethod(AppDelegateClass, button_action_selector, "v@:@",
     function(self, cmd, sender)
-        print("button clicked")
+        local title = ffi.string(sender.title:UTF8String())
+        print(title .. " clicked")
     end)
 
 local appDelegate = objc.AppDelegate:alloc():init()
@@ -43,10 +65,13 @@ appDelegate:autorelease()
 NSApp:setDelegate(appDelegate)
 
 local menubar = objc.NSMenu:alloc():init()
+menubar:autorelease()
 local appMenuItem = objc.NSMenuItem:alloc():init()
+appMenuItem:autorelease()
 menubar:addItem(appMenuItem)
 NSApp:setMainMenu(menubar)
 local appMenu = objc.NSMenu:alloc():init()
+appMenu:autorelease()
 
 local quitMenuItem = objc.NSMenuItem:alloc():initWithTitle_action_keyEquivalent(NSStr("Quit"), "terminate:",
     NSStr("q"))
@@ -65,14 +90,28 @@ local styleMask = bit.bor(NSWindowStyleMaskTitled, NSWindowStyleMaskClosable, NS
     NSWindowStyleMaskResizable)
 local window = objc.NSWindow:alloc():initWithContentRect_styleMask_backing_defer(rect, styleMask,
     NSBackingStoreBuffered, NO)
+window:autorelease()
 
-local button = objc.NSButton:alloc():initWithFrame(rect)
-button:setTitle(NSStr("Hello World"))
+local textField = objc.NSTextField:alloc():init()
+textField.placeholderString = NSStr("Enter Lua Code...")
+local button = objc.NSButton:buttonWithTitle_target_action(NSStr("Eval"), appDelegate, button_action_selector)
 
+local hStack = objc.NSStackView:alloc():init()
+hStack:autorelease()
+hStack:addView_inGravity(textField, NSStackViewGravityLeading)
+hStack:addView_inGravity(button, NSStackViewGravityTrailing)
 
-button.target = appDelegate
-button.action = "myClicked:"
-window.contentView = button
+local textView = objc.NSTextView:alloc():init()
+textView:autorelease()
+
+local vStack = objc.NSStackView:alloc():init()
+vStack:autorelease()
+vStack.orientation = NSUserInterfaceLayoutOrientationVertical
+vStack.edgeInsets = ffi.new("NSEdgeInsets", { top = 10, left = 10, bottom = 10, right = 10 })
+vStack:addView_inGravity(textView, NSStackViewGravityTop)
+vStack:addView_inGravity(hStack, NSStackViewGravityBottom)
+
+window.contentView = vStack
 
 window:setTitle(NSStr("LuaREPL"))
 window:makeKeyAndOrderFront(window)
