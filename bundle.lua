bfd4a1e59782b8e132ba1861901288bcc0539be8
Michael Mogenson <michael.mogenson@gmail.com>
2025-06-22 14:04:59 -0400

    compile lua bytecode

--- bundle.lua
+++ bundle.lua
@@ -2,22 +2,26 @@
 
 local package_preload = 'package.preload["%s"] = function()\n%s\nend\n'
 local require_pattern = [[require%s*%(?%s*(['"])(.-)%1%s*%)?]]
+local loader_script = '#!/bin/sh\ntail -n +4 "$0" | %s -\nexit\n'
 
 if not arg[1] or arg[1]:match("^-+h") then
     print(string.format("Usage: %s main.lua", arg[0]))
-    print("  Bundles all required Lua modules into one file")
-    print("  Outputs 'bundled_main.lua'")
-    print("  Does not support C modules")
+    print("  Bundles all Lua dependencies into one source by following required module paths.")
+    print("  Bundled source is then compiled to bytecode using the same Lua interpreter running this script.")
+    print("  A shell script loader and shebang are prepended to the bytecode so the file can be executed in place.")
+    print("  Outputs 'main.luac' or 'main.jlbc' depending on if this script is run with Lua or LuaJIT.")
+    print("  Supports Lua modules, no C modules, and Unix only.")
     os.exit()
 end
 
 local main_file = arg[1]
 local main_content = assert(io.open(main_file, "r")):read("*a")
-local output_name = "bundled_" .. main_file
+local output_name = main_file:gsub("%.[^.]*$", "") .. (jit and ".ljbc" or ".luac")
 local output_file = assert(io.open(output_name, "w"))
 
 local preload_modules = {}
 local skipped_modules = {}
+local bundled_sources = {}
 
 local function process_modules(content)
     for _, module_name in content:gmatch(require_pattern) do
@@ -41,9 +45,13 @@ end
 process_modules(main_content)
 
 for module_name, module_content in pairs(preload_modules) do
-    output_file:write(package_preload:format(module_name, module_content))
+    table.insert(bundled_sources, package_preload:format(module_name, module_content))
 end
+table.insert(bundled_sources, main_content)
 
-output_file:write(main_content)
+output_file:write(loader_script:format(jit and "luajit" or "lua"))
+output_file:write(string.dump(assert(load(table.concat(bundled_sources, "\n"))), true))
+output_file:close()
 
+os.execute("chmod +x " .. output_name)
 print("bundled into file: ", output_name)
