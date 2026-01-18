c67b67ebbf8190c10ddb92d8e456d5c08bc5c9fd
Michael Mogenson <michael.mogenson@gmail.com>
2026-01-18 12:28:35 -0500

    Update CI

--- shell.nix
+++ shell.nix
@@ -20,13 +20,17 @@ let
 
   lua = pkgs.luajit.withPackages (p: [ p.busted p.penlight json-lua ]);
 in pkgs.mkShell {
-  buildInputs = [ pkgs.curl pkgs.libuv lua ];
+  buildInputs = [ pkgs.curl pkgs.libuv lua pkgs.lua-language-server ];
 
   # Optional: Set up environment variables for Lua
   # LUA_PATH = "${lua}/share/lua/${pkgs.lib.getVersion lua}/?.lua;;";
   # LUA_CPATH = "${lua}/lib/lua/${pkgs.lib.getVersion lua}/?.so;;";
 
   shellHook = ''
-    echo "LuaJIT version: $(luajit -v)"
+    export DYLD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
+      pkgs.curl
+      pkgs.libuv
+    ]}:$DYLD_LIBRARY_PATH"
+    echo "Using LuaJIT version: $(luajit -v) with lib path: $DYLD_LIBRARY_PATH"
   '';
 }
