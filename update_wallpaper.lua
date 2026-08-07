14a1c27e5e0d155c169168ff3c8ff555df576714
Michael Mogenson <michael.mogenson@gmail.com>
2026-08-07 11:36:16 -0400

    bundle update_wallpaper

--- update_wallpaper.lua
+++ update_wallpaper.lua
@@ -1,5 +1,3 @@
-#!/usr/bin/env luajit
-package.path = package.path .. ";" .. os.getenv("HOME") .. "/?.lua"
 local curl = require("curl")
 
 local json, ret = curl.http_get("http://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1")
@@ -13,7 +11,7 @@ local _, _, path = json:find(pattern)
 local url = "https://bing.com" .. path .. "_UHD.jpg" ---@type string
 print("setting wallpaper to: " .. url)
 
-os.execute("echo $(date) >> $HOME/wallpaper.log")
-os.execute("echo " .. url .. " >> $HOME/wallpaper.log")
+os.execute("echo $(date) >> $HOME/.cache/wallpaper/wallpaper.log")
+os.execute("echo " .. url .. " >> $HOME/.cache/wallpaper/wallpaper.log")
 os.execute("termux-wallpaper -u " .. url .. " >> $HOME/wallpaper.log")
 os.execute("termux-wallpaper -l -u " .. url .. " >> $HOME/wallpaper.log")
